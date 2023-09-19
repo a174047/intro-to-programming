59eba411ab938dfa47e5db96a1eceec16edf7b30
Joseph Nam <joseph_nam@progressive.com>
2023-09-19 12:30:31 -0400

    Some test work

--- src/Week1/BankingSolution/Banking.Domain/Account.cs
+++ src/Week1/BankingSolution/Banking.Domain/Account.cs
@@ -2,8 +2,14 @@
 
 public class Account
 {
+    private decimal _balance = 5000M;
+    public void Deposit(decimal amountToDeposit)
+    {
+        _balance += amountToDeposit;
+    }
+
     public decimal GetBalance()
     {
-        return 5000M;
+        return _balance;
     }
 }
