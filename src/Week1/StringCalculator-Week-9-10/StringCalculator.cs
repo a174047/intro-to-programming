c0d697ee17d8db41eb67ccd148321e2c3ecd3ff9
Joseph Nam <joseph_nam@progressive.com>
2023-09-20 15:22:33 -0400

    string calculator

---
+++ src/Week1/StringCalculator-Week-9-10/StringCalculator.cs
@@ -0,0 +1,29 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (numbers.Length == 0) return 0;
+
+        List<char> delimetersList = new List<char> { ',', '\n' };
+
+        int startOfString = numbers.StartsWith("/") ? 4 : 0;
+        if (startOfString == 4)
+        {
+            delimetersList.Add(numbers[2]);
+        }
+
+        return numbers.Substring(startOfString).Split(delimetersList.ToArray()).Select(int.Parse).Sum();
+
+        /*int sum = 0;
+        foreach (var number in numberList)
+        {
+            sum += int.Parse(number);
+        }
+
+        return sum;*/
+    }
+}
---
+++ src/Week1/StringCalculator-Week-9-10/StringCalculatorTests.cs
@@ -0,0 +1,79 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("12", 12)]
+    [InlineData("108", 108)]
+    public void SingleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("108,2", 110)]
+    public void TwoDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2,3", 6)]
+    [InlineData("10,10,10,10,10", 50)]
+    [InlineData("100,34,1,0", 135)]
+    public void MultipleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2,3", 6)]
+    [InlineData("10\n20", 30)]
+    [InlineData("100\n34\n1\n0", 135)]
+    public void NewLineDelimeter(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("//;\n1;2", 3)]
+    [InlineData("//;\n10\n20;30,40", 100)]
+    [InlineData("//#\n100#34#1#0", 135)]
+    public void CustomDelimeters(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
