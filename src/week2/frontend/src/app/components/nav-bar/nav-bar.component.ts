07c0b0aea0231d401e039a13f3647821174387ba
Joseph Nam <joseph_nam@progressive.com>
2023-09-26 12:38:52 -0400

    before lunch

--- src/week2/frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ src/week2/frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -9,7 +9,7 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   template: `
     <div class="navbar bg-base-100">
       <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a class="btn btn-ghost normal-case text-xl">Frontend</a>
       </div>
       <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
@@ -25,9 +25,9 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
           </li>
           <li>
             <details>
-              <summary>Parent</summary>
+              <summary>Demos</summary>
               <ul class="p-2 bg-base-100">
-                <li><a>Link 1</a></li>
+                <li><a routerLink="todos">ToDos</a></li>
                 <li><a>Link 2</a></li>
               </ul>
             </details>
